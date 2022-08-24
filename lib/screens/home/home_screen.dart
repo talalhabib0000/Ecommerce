23f7feea791341f38df7b607b26e3bff8981311d
Talal <talalhabib0000@outlook.com>
2022-08-24 17:55:25 +0500

    Product Carousel with Stack and ListView

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/category_model.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -15,9 +18,26 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Bapco App'),
-      bottomNavigationBar: customNavBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Bapco App'),
+      bottomNavigationBar: const customNavBar(),
+      body: Column(
+        children: [
+          Container(
+            child: CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 1.5,
+                  viewportFraction: 0.9,
+                  enlargeCenterPage: true,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                ),
+                items: Category.categories
+                    .map((category) => HeroCarouelCard(category: category))
+                    .toList()),
+          ),
+          const SectionTile(title: 'Recommended')
+        ],
+      ),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,4 @@
 export 'custom_appbar.dart';
 export 'custom_navbar.dart';
+export 'hero_carousel_card.dart';
+export 'section_tile.dart';
